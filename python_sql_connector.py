43a9c99dafd104c24ae870ad0f65295f546edd11
Nischal Ashok <41947720+Nish-19@users.noreply.github.com>
2020-03-10 13:12:28 +0530

    Updated python_sql_connector.py
    Outputs only the unique element from the sql output.

--- python_sql_connector.py
+++ python_sql_connector.py
@@ -2,11 +2,12 @@ import mysql.connector as mysql
 
 def initialise_database_connection():
 	db = mysql.connect(#establishing connection with mysql
-    	host = "hostname",
-    	user = "username",
-    	database = 'databasename',
-    	password = 'password',
+    	host = "LAPTOP-P9CM599B",
+    	user = "check",
+    	database = 'innov_lab',
+    	password = '0123',
     	auth_plugin='caching_sha2_password'
+    	#it is good practice to include the passwd as well.
 	)#update queries will work only if we provide the password while connecting.
 	print ("prompt from the mySQL connector : ",db)
 	return db
@@ -17,4 +18,6 @@ def generate_output(sql_query, db):
 	cursor.execute("USE innov_lab;")#selecting the database to work work
 	cursor.execute(sql_query)#fire the query
 	returnedData = cursor.fetchall()#fetch the result from the connector
-	print(returnedData)
+	unique_set = set(returnedData)
+	unique_list = list(unique_set)
+	print(unique_list)
